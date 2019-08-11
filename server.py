1a27a546b769cba66f0815fd40f92158ebddfe46
rautis <aapo@pynnonsaari.net>
2019-08-11 09:21:02 +0300

    modified server to use the model with filter data. Small fixes and improvements.

--- server.py
+++ server.py
@@ -12,11 +12,11 @@ import joblib
 app = Flask(__name__)
 api = Api(app)
 
-model = load_model("models/lstm-weather-data-look-back-1.h5")
+model = load_model("models/lstm-filter-data-look-back-8.h5")
 graph = tf.get_default_graph()
 
-scaler_X = joblib.load("models/scaler_X.pkl")
-scaler_y = joblib.load("models/scaler_y.pkl")
+scaler_X = joblib.load("models/scaler_X_filter.pkl")
+scaler_y = joblib.load("models/scaler_y_filter.pkl")
 
 class AirQualityIndex(Resource):
     
@@ -24,33 +24,23 @@ class AirQualityIndex(Resource):
         return { "status": "success", "message": "Use POST API to pass the previous hour data" }
     
     def post(self):
-        #Mannerheimintie	Air pressure (msl) (hPa)	
-        #Relative humidity (%)	Rain intensity (mm/h)	
-        #Air temperature (degC)	Wind direction (deg)	
-        #Wind speed (m/s)
+        
         global graph
         with graph.as_default():
-            X = np.array([[
-                request.json["paqi-1"],
-                request.json["ap-1"],
-                request.json["rh-1"],
-                request.json["ri-1"],
-                request.json["at-1"],
-                request.json["wd-1"],
-                request.json["ws-1"],
-            ],
-            [
-                request.json["paqi-2"],
-                request.json["ap-2"],
-                request.json["rh-2"],
-                request.json["ri-2"],
-                request.json["at-2"],
-                request.json["wd-2"],
-                request.json["ws-2"],
-            ]])
-            
+
+            # Get input data
+            data = request.json["data"]
+            entris = []
+
+            for d in data:
+                entris.append(d["paqi"])
+                entris.append(d["rm"])
+                entris.append(d["rstd"])
+                entris.append(d["wiener"])
+
+            X = np.array(entris).reshape(8,4)            
             X = scaler_X.transform(X)
-            X = X.reshape(1,2,7)
+            X = X.reshape(1,8,4)
 
             yhat = scaler_y.inverse_transform(model.predict(X))
 
